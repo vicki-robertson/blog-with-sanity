38ac5b9fdbb372e70c7848124c5ff41f08bc8c92
Vicki Robertson <vicki-robertson@users.noreply.github.com>
2025-07-11 00:11:13 +0200

    Add sanity fetch and basic queries

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main className="min-h-screen p-24">
-      <h1>hello</h1>
+      <h1 className="text-5xl text-center font-semibold">The Sketch Burrow</h1>
     </main>
   );
 }
