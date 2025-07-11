639208ca475b89a6b10cf59f496dfceee3ab1270
Vicki Robertson <vicki-robertson@users.noreply.github.com>
2025-07-11 14:07:57 +0200

    feat: add basic posts component and page to display individual blog posts from sanity"

---
+++ app/components/Post.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import { SanityDocument } from "@sanity/client";
+import { PortableText } from "@portabletext/react";
+import { client } from "@/sanity/lib/client";
+import ImageUrlBuilder from "@sanity/image-url";
+import Image from "next/image";
+
+const builder = ImageUrlBuilder(client);
+
+const Post = ({ post }: { post: SanityDocument }) => {
+  return (
+    <main>
+      <h1>{post.title}</h1>
+      {post?.mainImage ? (
+        <Image
+          src={builder.image(post.mainImage).width(600).height(600).url()}
+          alt={post?.mainImage?.alt}
+          width={600}
+          height={600}
+        />
+      ) : null}
+      {post?.body ? <PortableText value={post.body} /> : null}
+    </main>
+  );
+};
+
+export default Post;
